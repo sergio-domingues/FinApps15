f17129058e015f8856fc3cc03adbc7890a9a073a
444Duarte <duartemr.pinto@gmail.com>
2015-11-06 21:44:13 +0100

    Added gradle and finished model slide list

---
+++ android/app/src/main/java/coderdudos/printdonation/DownloadImage.java
@@ -0,0 +1,10 @@
+package coderdudos.printdonation;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public interface DownloadImage {
+
+    void downloadImage(ImageView imagePlace, final View view, String url);
+}
